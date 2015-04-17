56bb8a02cc374566b211037789fd1a143ebf186e
Cochet <jielpe99@gmail.com>
2015-04-17 15:16:58 +0200

    maj

--- src/fr/mastermind/MainMastermind.java
+++ src/fr/mastermind/MainMastermind.java
@@ -7,15 +7,25 @@ public class MainMastermind {
 	public static void main(String[] args) {
 		// Jeu Mastermind
 		
-		// D�claration des variables
+		// Declaration des variables
 		// feuille de parametres
 		Scanner sc = new Scanner(System.in);
 		MastermindParam param = new MastermindParam();
-		String indice[] = new String[0]; // Tableau indiquant la situation du joueur en termes de pions bien ou mal places
-		int joueur[] = new int[0]; // Declaration du tableau du joueur
-		int mm[] = new int[0]; // Declaration du tableau utilise par l'ordinateur
-		int val[] = new int[0]; // Choix des pions
-		int compteurDeFautes = 0; // Declaration du compteur de fautes, on initialise a 5 pour entrer dans la boucle
+		
+		// Tableau indiquant la situation du joueur en termes de pions bien ou mal places
+		String indice[] = new String[0];
+		
+		// Declaration du tableau du joueur
+		int joueur[] = new int[0];
+		
+		// Declaration du tableau utilise par l'ordinateur
+		int mm[] = new int[0]; 
+		
+		// Choix des pions
+		int val[] = new int[0]; 
+		
+		// Declaration du compteur de fautes, on initialise a 5 pour entrer dans la boucle
+		int compteurDeFautes = 0; 
 		
 		
 		/*
@@ -26,21 +36,21 @@ public class MainMastermind {
 		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%");
 		
 		System.out.println("Bienvenue dans ce jeu de mastermind !");
-		System.out.println("Avant de commencer, vous allez choisir vos règles.");
-		System.out.println("Vous allez choisir : le nombre d'emplacements, le nombre de couleurs de pions (representé ici par des chiffres)");
-		System.out.println("Et enfin le bombre d'essais que vous vous donnez pour trouver la bonne combinaison.");
+		System.out.println("Avant de commencer, vous allez choisir vos regles.");
+		System.out.println("Vous allez choisir : le nombre d'emplacements, le nombre de couleurs de pions (represente ici par des chiffres)");
+		System.out.println("Et enfin le nombre d'essais que vous vous donnez pour trouver la bonne combinaison.");
 		System.out.println("Avec combien d'emplacements de pions voulez vous jouer ?");
-		param.nbPions = sc.nextInt();
+		param.setNbPions( sc.nextInt() );
 		System.out.println("Avec combien de 'couleurs' de pions voulez vous jouer ?");
-		param.nbCategoriesPions = sc.nextInt();
+		param.setNbCategoriesPions ( sc.nextInt() );
 		System.out.println("Combien d'essais max vous laissez vous pour trouver la bonne combinaison ?");
-		param.nbEssaisMax = sc.nextInt();
+		param.setNbEssaisMax ( sc.nextInt() );
 		System.out.println("L'ordinateur à placé ses pions, à vous de jouer ! Bon courage !");
-		indice = new String[param.nbPions];
-		joueur = new int[param.nbPions];
-		mm = new int[param.nbPions];
-		val = new int[param.nbCategoriesPions];
-		compteurDeFautes = param.nbPions;
+		indice = new String[param.getNbPions()];
+		joueur = new int[param.getNbPions() ];
+		mm = new int[param.getNbPions() ];
+		val = new int[param.getNbCategoriesPions() ];
+		compteurDeFautes = param.getNbPions() ;
 		Tableau.parcourirTab(mm);
 		
 		/*
--- src/fr/mastermind/MastermindParam.java
+++ src/fr/mastermind/MastermindParam.java
@@ -2,10 +2,18 @@ package fr.mastermind;
 
 public class MastermindParam {
 	// Choix des r�gles : on pourra choisir le nombre de pions et le nombre de pions diff�rents
-	int nbPions; // Nombre de pion
-	int nbCategoriesPions; // Nombre de categories de pions
-	int nbEssaisMax; // Le joueur doit trouver la solution en nbEssaisTotal fois
-	int[] tabCatPions = new int[10];// Tableau rempli des diff�rentes cat�rorie ex: {1,2,3,4}
+	
+	// Nombre de pion
+	private int nbPions; 
+	
+	// Nombre de categories de pions
+	private int nbCategoriesPions; 
+	
+	// Le joueur doit trouver la solution en nbEssaisTotal fois
+	private int nbEssaisMax; 
+	
+	// Tableau rempli des differentes caterories ex: {1,2,3,4}
+	private int[] tabCatPions = new int[10];
 	
 	/**
 	 * Methode qui prend en argument le nb de categories de pions defini par l'utilisateur
@@ -29,11 +37,55 @@ public class MastermindParam {
 	 }
 	 
 	 // Creation d'un jeu de parametre
-	 public MastermindParam( int pNbPions, int pNbCategoriesPions, int pNbEssaisMax)
+	 public MastermindParam( int NbPions, int NbCategoriesPions, int NbEssaisMax)
 	 {
-		 nbPions = pNbPions;
-		 nbCategoriesPions = pNbCategoriesPions;
-		 nbEssaisMax = pNbEssaisMax;
-		 tabCatPions =  creationTabCat ( pNbCategoriesPions );
+		 this.nbPions = NbPions;
+		 this.nbCategoriesPions = NbCategoriesPions;
+		 this.nbEssaisMax = NbEssaisMax;
+		 this.tabCatPions =  creationTabCat ( NbCategoriesPions );
+	 }
+	 
+	 // Accesseur et getteur de nbPions
+	 public void setNbPions ( int nbPions )
+	 {
+		 this.nbPions = nbPions;
+	 }
+	 
+	 public int getNbPions ( )
+	 {
+		 return this.nbPions;
+	 }
+	 
+	 // Accesseur et getteur de nbCategoriesPions
+	 public void setNbCategoriesPions ( int nbCategoriesPions )
+	 {
+		 this.nbCategoriesPions = nbCategoriesPions;
+	 }
+	 
+	 public int getNbCategoriesPions ( )
+	 {
+		 return this.nbCategoriesPions;
+	 }
+	 
+	 // Accesseur et getteur de nbEssaisMax
+	 public void setNbEssaisMax ( int nbEssaisMax )
+	 {
+		 this.nbEssaisMax = nbEssaisMax;
+	 }
+	 
+	 public int getNbEssaisMax ( )
+	 {
+		 return this.nbEssaisMax;
+	 }
+	 
+	 // Accesseur et getteur de tabCatPions
+	 public void setTabCatPions ( int [] tabCatPions )
+	 {
+		 this.tabCatPions = tabCatPions;
+	 }
+	 
+	 public int []  getTabCatPions ( )
+	 {
+		 return this.tabCatPions;
 	 }
 }
